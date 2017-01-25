26e608694049449ecc98da79dadfdb85d3adc442
Armin <armin.ag@outlook.com>
2017-01-25 16:59:08 +0100

    Add Category Initialization on MainFragment

--- app/src/main/java/solutions/hedron/simple_store/fragments/MainFragment.java
+++ app/src/main/java/solutions/hedron/simple_store/fragments/MainFragment.java
@@ -3,6 +3,7 @@ package solutions.hedron.simple_store.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -27,8 +28,29 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+
+        // Add Games Category
+        fragmentManager
+                .beginTransaction()
+                .add(R.id.containerTopRow, CategoryFragment.newInstance(CategoryFragment.CATEGORY_TYPE_GAMES))
+                .commit();
+
+        // Add Productivity Category
+        fragmentManager
+                .beginTransaction()
+                .add(R.id.containerMiddleRow, CategoryFragment.newInstance(CategoryFragment.CATEGORY_TYPE_PRODUCTIVITY))
+                .commit();
+
+        // Add Health Category
+        fragmentManager
+                .beginTransaction()
+                .add(R.id.containerBottomRow, CategoryFragment.newInstance(CategoryFragment.CATEGORY_TYPE_HEALTH))
+                .commit();
+
+        return view;
     }
 
 }
