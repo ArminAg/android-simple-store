9a1b9d4e6608f654159c3a8c0be50006e3d16473
Armin <armin.ag@outlook.com>
2017-01-25 16:05:19 +0100

    Add MainFragment for initial loading

--- app/src/main/java/solutions/hedron/simple_store/MainActivity.java
+++ app/src/main/java/solutions/hedron/simple_store/MainActivity.java
@@ -1,13 +1,41 @@
 package solutions.hedron.simple_store;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import solutions.hedron.simple_store.fragments.MainFragment;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static MainActivity mainActivity;
+
+    public static MainActivity getMainActivity(){
+        return mainActivity;
+    }
+
+    public static void setMainActivity(MainActivity mainActivity){
+        MainActivity.mainActivity = mainActivity;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MainActivity.setMainActivity(this);
+
+        if (getSupportFragmentManager().findFragmentById(R.id.fragmentsContainer) == null) {
+            loadMainScreen();
+        }
+    }
+
+    public void loadMainScreen(){
+        getSupportFragmentManager()
+                .beginTransaction()
+                .add(R.id.fragmentsContainer, MainFragment.newInstance())
+                .commit();
     }
 }
---
+++ app/src/main/java/solutions/hedron/simple_store/fragments/MainFragment.java
@@ -0,0 +1,34 @@
+package solutions.hedron.simple_store.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import solutions.hedron.simple_store.R;
+
+public class MainFragment extends Fragment {
+    public MainFragment() {
+        // Required empty public constructor
+    }
+
+    public static MainFragment newInstance() {
+        MainFragment fragment = new MainFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+}
